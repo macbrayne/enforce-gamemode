4d6b6108eed4a5c61a0e890cc13681ca5566e87b
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2022-01-14 10:20:50 +0100

    add mixin hooking into set gamemode instead of checking the gamemode every tick change data breakers modRuntime to modRuntimeOnly

--- src/main/java/de/macbrayne/fabric/forcegamemode/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/de/macbrayne/fabric/forcegamemode/mixin/ServerPlayerEntityMixin.java
@@ -2,6 +2,8 @@ package de.macbrayne.fabric.forcegamemode.mixin;
 
 import de.macbrayne.fabric.forcegamemode.GameModePermission;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin {
 	@Unique
+	private static final Logger LOGGER = LoggerFactory.getLogger("ForceGamemode");
+
+	@Unique
 	@Inject(method = "playerTick", at = @At("HEAD"))
 	private void forceGamemode$onPlayerTick(CallbackInfo ci) {
 		ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
@@ -18,7 +23,9 @@ public abstract class ServerPlayerEntityMixin {
 		for (var entry : GameModePermission.PERMISSIONS.entrySet()) {
 			GameModePermission permission = entry.getValue();
 			if (permission.check(player)) {
-				player.changeGameMode(entry.getKey());
+				if(player.changeGameMode(entry.getKey())) {
+					LOGGER.debug("Updated {}'s game mode to {} due to {}", player.getName().asString(), entry.getKey().getName(), permission.getPermission());
+				}
 				break;
 			}
 		}
