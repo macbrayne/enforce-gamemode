e059a51230650e0d468b2aa31b55a53f65315fb4
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2022-01-15 01:15:38 +0100

    tweak metadata: - remove unused mod entrypoint - set mod as server only - move mixins to server only due permissions mods generally being server only - tweak description

--- src/main/java/de/macbrayne/fabric/enforcegamemode/EnforceGamemode.java
+++
@@ -1,9 +0,0 @@
-package de.macbrayne.fabric.enforcegamemode;
-
-import net.fabricmc.api.ModInitializer;
-
-public class EnforceGamemode implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
-}
